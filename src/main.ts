89299ce95eff6cbaac9148e3dcc5b8842e1b0c74
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-07-10 18:51:31 +0900

    리엑트, 네스트 환경 구축

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // React 애플리케이션의 정적 파일 제공을 위한 설정
+   app.use(express.static(join(__dirname, '..', 'client/build')));
+
   await app.listen(3000);
 }
 bootstrap();
